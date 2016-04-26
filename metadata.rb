28a9ce737241081953192c91b51967f38eac2ae7
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-04-26 20:05:41 +0400

    Draft

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'themis-finals-lib-attack-result-rb'
+description 'Installs and configures attack-result-rb'
+version '1.0.0'
+
+recipe 'themis-finals-lib-attack-result-rb', 'Installs and configures attack-result-rb'
+depends 'latest-git', '~> 1.1.6'
+depends 'git2', '~> 1.0.0'
