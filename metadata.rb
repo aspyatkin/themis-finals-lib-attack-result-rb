a606d59dc85483431d5c200b30b7a988a3ccef07
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-08-06 14:12:50 +0400

    Replace symbolized keys

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-lib-attack-result-rb'
 description 'Installs and configures attack-result-rb'
-version '1.0.2'
+version '1.0.3'
 
 recipe 'themis-finals-lib-attack-result-rb', 'Installs and configures attack-result-rb'
 depends 'latest-git', '~> 1.1.6'
