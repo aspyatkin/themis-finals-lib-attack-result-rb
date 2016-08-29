01e60603e424f3ce13e034a2c97731e53eb8ecb9
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-08-29 19:28:05 +0400

    Change repository URL

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'themis-finals-lib-attack-result-rb'
 description 'Installs and configures attack-result-rb'
-version '1.0.3'
+version '1.0.4'
 
 recipe 'themis-finals-lib-attack-result-rb', 'Installs and configures attack-result-rb'
-depends 'latest-git', '~> 1.1.6'
+depends 'latest-git', '~> 1.1.11'
 depends 'git2', '~> 1.0.0'
