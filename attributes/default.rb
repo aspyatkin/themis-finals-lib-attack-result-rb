a16cff7e489232872946dffe865ae930671e5daa
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-09-04 21:04:45 +0400

    Make use of instance cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,5 @@
 id = 'themis-finals-lib-attack-result-rb'
 
 default[id]['basedir'] = '/var/themis/finals/lib/attack-result-rb'
-default[id]['user'] = 'vagrant'
-default[id]['group'] = 'vagrant'
 default[id]['github_repository'] = 'themis-project/themis-finals-attack-result-rb'
 default[id]['revision'] = 'master'
--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name 'themis-finals-lib-attack-result-rb'
 description 'Installs and configures attack-result-rb'
-version '1.1.1'
+version '1.2.0'
 
 recipe 'themis-finals-lib-attack-result-rb', 'Installs and configures attack-result-rb'
-depends 'git', '~> 6.0.0'
+depends 'git', '~> 8.0.0'
 depends 'git2', '~> 1.0.0'
 depends 'ssh-private-keys', '~> 2.0.0'
-depends 'ssh_known_hosts', '~> 5.1.0'
+depends 'ssh_known_hosts', '~> 4.0.0'
+depends 'instance', '~> 1.0.0'
